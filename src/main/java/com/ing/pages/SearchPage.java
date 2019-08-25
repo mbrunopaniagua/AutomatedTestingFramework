339b25aebb1f1e95f05b7f5214d83962f6955550
Mario Bruno <mbruno@idealista.com>
2019-08-26 00:06:24 +0200

    Create SearchPage and refactor

---
+++ src/main/java/com/ing/pages/SearchPage.java
@@ -0,0 +1,19 @@
+package com.ing.pages;
+
+import com.ing.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchPage {
+    private WebDriver driver = DriverFactory.getDriver();
+
+    public void sortedByMostVoted() {
+        driver.findElement(By.xpath("//*[@id=\"mainbar\"]/div[4]/div/div[2]/div/div[3]/button")).click();
+        driver.findElement(By.xpath("//input[@type='radio'][@value='MostVotes']")).click();
+        driver.findElement(By.xpath("//*[@id=\"uql-form\"]/div/div/div[2]/div/div[1]/button")).click();
+    }
+
+    public void selectPost() {
+        driver.findElement(By.xpath("//*[@id=\"question-summary-11227809\"]/div[2]/h3/a")).click();
+    }
+}
--- src/test/java/com/ing/SearchTest.java
+++ src/test/java/com/ing/SearchTest.java
@@ -1,6 +1,7 @@
 package com.ing;
 
 import com.ing.pages.HomePage;
+import com.ing.pages.SearchPage;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,18 +15,14 @@ import java.io.IOException;
 public class SearchTest extends BaseTest {
 
     private HomePage home = new HomePage();
+    private SearchPage search = new SearchPage();
 
     @Test
     public void test() throws IOException {
         home.search("java");
 
-        //filter by most votes
-        driver.findElement(By.xpath("//*[@id=\"mainbar\"]/div[4]/div/div[2]/div/div[3]/button")).click();
-        driver.findElement(By.xpath("//input[@type='radio'][@value='MostVotes']")).click();
-        driver.findElement(By.xpath("//*[@id=\"uql-form\"]/div/div/div[2]/div/div[1]/button")).click();
-
-        //select most voted post
-        driver.findElement(By.xpath("//*[@id=\"question-summary-11227809\"]/div[2]/h3/a")).click();
+        search.sortedByMostVoted();
+        search.selectPost();
 
         //verify title
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@id=\"question\"]/div[2]/div[1]/div/button[1]")).isDisplayed());
